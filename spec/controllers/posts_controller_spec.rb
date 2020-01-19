ceea7cf71a441df7c02c9304d43f065a9305f4af
Alastair Gilles <agilles91@gmail.com>
2020-01-19 13:04:36 +0100

    fix: make it not possible to post without loggin in

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,11 @@
 describe PostsController, type: :controller do
+  describe 'GET #new' do
+    it 'redirects to log in if not logged in' do
+      get :new
+      expect(response).to redirect_to(new_user_session_path)
+    end
+  end
+
   describe 'POST #create' do
     it 'creates a post with an image' do
       user = User.create email: 'postcreate@example.com', password: '123456'
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
+  it 'should not have a new post button if not logged in' do
+    assign(:posts, [])
+    allow(view).to receive(:user_signed_in?).and_return false
+
+    render
+
+    expect(rendered).not_to have_selector 'a', text: 'New post'
+  end
 end
