2ce618448103a6c25bfc465ffd4175ec96d841f6
Alastair Gilles <agilles91@gmail.com>
2020-01-19 17:20:28 +0100

    test: add test for nav links when logged in

--- spec/features/log_in_out_buttons_spec.rb
+++ spec/features/log_in_out_buttons_spec.rb
@@ -9,4 +9,17 @@ describe 'Session buttons', type: :feature do
       expect(page).not_to have_selector 'a', text: 'Log out'
     end
   end
+
+  describe 'when logged in' do
+    it 'should show options to log out' do
+      sign_up
+
+      visit root_path
+
+      expect(page).not_to have_selector 'a', text: 'Sign up'
+      expect(page).not_to have_selector 'a', text: 'Sign in'
+
+      expect(page).to have_selector 'a', text: 'Log out'
+    end
+  end
 end
