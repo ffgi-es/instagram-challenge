00912cd7a26bc1f237e3efb0fe0220ab0a3deb0f
Alastair Gilles <agilles91@gmail.com>
2020-01-19 17:16:36 +0100

    fix: add links conditionally for sessions in layout

---
+++ spec/features/log_in_out_buttons_spec.rb
@@ -0,0 +1,12 @@
+describe 'Session buttons', type: :feature do
+  describe 'when not logged in' do
+    it 'should show options to log in or sign up' do
+      visit root_path
+
+      expect(page).to have_selector 'a', text: 'Sign up'
+      expect(page).to have_selector 'a', text: 'Sign in'
+
+      expect(page).not_to have_selector 'a', text: 'Log out'
+    end
+  end
+end
